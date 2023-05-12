88afc48fde12799b6009d627bb8d4ceb0809dccd
Alok Sharma <67829527+aloksharma-1@users.noreply.github.com>
2023-05-12 13:07:13 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,50 @@
+import string
+import streamlit as st
+import pandas as pd
+import pickle
+
+# Load the trained model
+model = pickle.load(open('pipe.pkl','rb'))
+
+# Set the app title
+st.title('Machine Learning Model')
+
+guidelines = 'Input for "Type" of Transaction is not case sensitive,\nThe allowed inputs are:- \n["payment", "transfer", "cash_in", "cash_out", "debit"]'
+st.warning(guidelines)
+# Create input fields for user to enter values
+
+ttype= st.text_input('Type')
+amount = st.text_input('Amount:')
+oldbalance = st.text_input('Old Balance:')
+newbalance = st.text_input('New Balance:')
+newbalanceDest=st.text_input('New Balance Dest:')
+oldbalanceDest=st.text_input('Old  Balance Dest:')
+
+ttype = ttype.upper()
+type_payment = ['PAYMENT', 'TRANSFER', 'CASH_IN', 'CASH_OUT', 'DEBIT']
+for i in range(len(type_payment)):
+    if ttype == type_payment[i]:
+        paymentType = i
+# Create a button to trigger the prediction
+if st.button('Predict'):
+    # Prepare the input data for prediction
+    data = pd.DataFrame({
+        'type':[paymentType],
+        'amount': [float(amount)],
+        'oldbalanceOrg': [float(oldbalance)],
+        'newbalanceOrig': [float(newbalance)],
+        'oldbalanceDest': [float(oldbalanceDest)],
+        'newbalanceDest': [float(newbalanceDest)],
+    })  
+    st.write(data)
+
+    # Make the prediction using the loaded model
+    prediction = model.predict(data)
+    if prediction[0] == 0.:
+        st.success('Transaction is Geniune !!')
+    else:
+        st.success('Transaction is Fishy')
+
+    # Display the prediction result
+    st.success('Prediction: {}'.format(prediction[0]))
+   
